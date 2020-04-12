285f5e2e83049ea63a766c152d71d5ca4a11e683
Jearce <jessiearce42@gmail.com>
2020-04-12 01:04:44 -0500

    Fixed DecimalFields and added doc strings.

--- source/edutravel/travel_agency/models.py
+++ source/edutravel/travel_agency/models.py
@@ -1,7 +1,10 @@
 from django.db import models
-#TODO add max_digits and decimial_places to DecimalField
 
 class Person(models.Model):
+    '''
+    Parent class Employee,Traveler, and InstituteFaculty.
+
+    '''
 
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
@@ -9,10 +12,11 @@ class Person(models.Model):
     passport  = models.CharField(max_length=80)
     phone_number = models.CharField(max_length=12)
 
-
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
 class Employee(Person):
     '''Employee table'''
@@ -20,15 +24,8 @@ class Employee(Person):
 
 class Institute(models.Model):
     '''
-
-    PK instituteID
-    InstituteName
-    StreetAddress
-    CityAddress
-    StateAddress
-    CountryAddress
-    Type
-    StudentCount
+    Institute can make many bookings and a booking
+    will be for one trip.
 
     '''
     name = models.CharField(max_length=50)
@@ -44,6 +41,10 @@ class Institute(models.Model):
 
 
 class InstituteType(models.Model):
+    '''
+    Axiliary table for Institute model.
+
+    '''
     institute_type = models.CharField(max_length=80)
 
     def __str__(self):
@@ -52,11 +53,13 @@ class InstituteType(models.Model):
 
 class Trip(models.Model):
     '''
+    Trip can have many bookings for it and that booking
+    is made by an institute.
 
     '''
     name = models.CharField(max_length=50)
     trip_organizer = models.ForeignKey(Employee,on_delete=models.CASCADE)
-    trip_expense = models.DecimalField()
+    trip_expense = models.DecimalField(max_digits=6,decimal_places=2)
     start_date = models.DateField()
     end_date = models.DateField()
     institutes = models.ManyToManyField(Institute,through="Booking")
@@ -70,6 +73,10 @@ class TripExpense(models.Model):
 
 
 class ExpenseType(models.Model):
+    '''
+    Axiliary table for TripExpense Model.
+
+    '''
 
     expense_type = models.CharField(max_length=80)
 
@@ -78,14 +85,20 @@ class ExpenseType(models.Model):
 
 
 class RequestForProposal(models.Model):
+    '''
+    Many request for proposals (RFP) can be
+    submitted by an institute. An RFP can have
+    many bids for a trip.
+
+    '''
 
     institute = models.ForeignKey(Institute,on_delete=models.CASCADE)
     name = models.CharField(max_length=80)
     submission_date = models.DateField()
     projected_date = models.DateField()
     rfp_status = models.CharField(max_length=80)
-    proposed_amount = models.DecimalField()
-    accepted_amount = models.DecimalField()
+    proposed_amount = models.DecimalField(max_digits=6,decimal_places=2)
+    accepted_amount = models.DecimalField(max_digits=6,decimal_places=2)
     trips = models.ManyToManyField(Trip,through="Bid")
 
     def __str__(self):
@@ -94,22 +107,33 @@ class RequestForProposal(models.Model):
 
 class Bid(models.Model):
     '''
+    Associative table
+
+    A request for proposal can have many bids for it and a bid will
+    be made for a trip.
 
     '''
     trip = models.ForeignKey(Trip,on_delete=models.CASCADE)
     rfp = models.ForeignKey(RequestForProposal,on_delete=models.CASCADE)
-    amount = models.DecimalField()
+    amount = models.DecimalField(max_digits=6,decimal_places=2)
     accepted = models.BooleanField()
 
 
 class PaymentPlan(models.Model):
+    '''
+    Axiliary table for Invoice model.
+
+    '''
     plan = models.CharField(max_length=80)
 
     def __str__(self):
         return self.plan
 
-
 class Invoice(models.Model):
+    '''
+    Many invoices can be made for a trip.
+
+    '''
     trip = models.ForeignKey(Trip,on_delete=models.CASCADE)
     total_amount = models.FloatField()
     paid = models.BooleanField()
@@ -117,20 +141,37 @@ class Invoice(models.Model):
 
 
 class PaymentStatus(models.Model):
+    '''
+
+    Axiliary table for Payment model.
+
+    '''
     status = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.status
 
 
 class Payment(models.Model):
+    '''
+    An invoice can have many payments.
+
+    '''
     invoice = models.ForeignKey(Invoice,on_delete=models.CASCADE)
-    amount = models.DecimalField()
+    amount = models.DecimalField(max_digits=6,decimal_places=2)
     payment_type = models.CharField(max_length=12)
     payment_status = models.ForeignKey(PaymentStatus,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.amount
 
 class Booking(models.Model):
     '''
-    FK InstitueID
-    FK TripID
+
+    Associative table
+
+    A booking belongs to a trip.
+
     '''
     name = models.CharField(max_length=120)
     institute = models.ForeignKey(Institute,on_delete=models.CASCADE)
@@ -143,14 +184,16 @@ class Booking(models.Model):
 
 class Traveler(Person):
     '''
+    A trip can have many travelers who register for the trip.
 
     '''
-    insitute = models.ForeignKey(Institute,on_delete=models.CASCADE)
+    institute = models.ForeignKey(Institute,on_delete=models.CASCADE)
     trips = models.ManyToManyField(Trip,through="TripRegistration")
 
 
 class TripHistory(models.Model):
     '''
+    A traveler will have a history of their trips.
 
     '''
     traveler  = models.ForeignKey(Traveler,on_delete=models.CASCADE)
@@ -161,12 +204,20 @@ class TripHistory(models.Model):
 class TripRegistration(models.Model):
     '''
     Associative table for Trip and Traveler.
+
+    A traveler can make many registrations and a registration will be
+    for a trip.
+
     '''
     trip = models.ForeignKey(Trip,on_delete=models.CASCADE)
     travler = models.ForeignKey(Traveler,on_delete=models.CASCADE)
 
 
 class Department(models.Model):
+    '''
+    Axiliary table for institute facultu model.
+
+    '''
 
     name = models.CharField(max_length=80)
 
@@ -176,22 +227,28 @@ class Department(models.Model):
 
 class InstituteFaculty(Person):
     '''
+    Child model of traveler
 
     '''
-    traveler = models.ForeignKey(Traveler,on_delete=models.CASCADE)
+    traveler = models.OneToOneField(Traveler,on_delete=models.CASCADE)
     title = models.CharField(max_length=80)
     department = models.ForeignKey(Department,on_delete=models.CASCADE)
 
 
 class Majors(models.Model):
     '''
+    Axiliary table for student model.
 
     '''
     major =  models.CharField(max_length=80)
 
+    def __str__(self):
+        return self.major
+
 
 class Student(Person):
     '''
+    Child model of traveler.
 
     '''
 
@@ -203,6 +260,6 @@ class Student(Person):
         ('GR','Graduate')
     ]
 
-    traveler = models.ForeignKey(Traveler,on_delete=models.CASCADE)
+    traveler = models.OneToOneField(Traveler,on_delete=models.CASCADE)
     declared_major = models.ForeignKey(Majors,on_delete=models.CASCADE)
     grade_level = models.CharField(max_length=2,choices=years_in_school)
