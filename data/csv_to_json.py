c7586e84708d5ff4f0fa514e59b30ba576b87fda
Jearce <jessiearce42@gmail.com>
2020-04-14 19:05:27 -0500

    Python script to convert one or more csv files to json file.

---
+++ data/csv_to_json.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import json
+import os
+
+import argparse
+
+def create_record(model_name,keys,values):
+    '''
+
+    Arguments:
+
+    model_name: name of the model
+    keys: header names as a list
+    values: values from a row as a list
+
+    Returns
+     A dict representing a record.
+    '''
+
+    pk = values[0]
+    record = {"model":'travel_agency.'+model_name,"pk":pk}
+    fields = {attribute:value for attribute,value in zip(keys[1:],values[1:])}
+    record['fields'] = fields
+
+    return record
+
+def csv_to_json(file):
+    '''
+    file: csv file name
+
+    Results:
+     writes a json file from a csv file.
+
+    '''
+    #open csv
+    csv_file = open(file)
+    #create json file
+    json_file = open(file.replace('.csv','.json'),'w')
+
+    header = csv_file.readline().strip().split(',')
+    model = file[:file.find('.')]
+
+    #get records in json format
+    records = []
+    for row in csv_file:
+        row = row.strip().split(',')
+        record = create_record(model,header,row)
+        records.append(record)
+
+    csv_file.close()
+
+    #write to json file
+    json_file.write(json.dumps(records))
+    json_file.close()
+
+def get_args():
+
+    parser = argparse.ArgumentParser(description="Convert CSV to json for Django dumpdata.")
+    parser.add_argument('infiles',
+                        nargs="*",
+                        help="CSV files to be converted to json.",
+                        metavar="One or more CSV files.",
+                        default=None)
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+
+    args = get_args()
+    if args.infiles:
+        for file in args.infiles:
+            csv_to_json(file)
+    else:
+        for file in os.list():
+            csv_to_json(file)
+
