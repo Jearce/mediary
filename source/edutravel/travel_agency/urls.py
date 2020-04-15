d3be43e60ebb793108fb7697960b8350fe3d99b7
Jearce <jessiearce42@gmail.com>
2020-04-15 14:23:50 -0500

    Added url patterns to allow for navigation from home page to other pages.

--- source/edutravel/travel_agency/urls.py
+++ source/edutravel/travel_agency/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
+    path("",views.home,name="home"),
+    path("plan",views.plan_trip,name="plan"),
+    path("create-account",views.create_account,name="create")
 ]
 
