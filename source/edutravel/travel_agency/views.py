5ab4df5f491416a9602ffb17fc13bf55e196b7af
Nick Holly <NicoholasHolly@gmail.com>
2020-04-16 08:23:06 -0500

    defined get_local_items function

--- source/edutravel/travel_agency/views.py
+++ source/edutravel/travel_agency/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
-from .models import Country,Subdivision,City
+from .models import Country,Subdivision,City,LocalTransportation, LocalGuide, LocalLodging, LocalAttraction
 
 # Create your views here.
 def home(request):
@@ -24,6 +24,12 @@ def get_cities(request):
     cities = City.objects.filter(subdivision=request.GET["state"])
     return render(request,'travel_agency/city-by-state.html',{'states':states, 'countries':countries,'cities':cities})
 
+def get_local_items(request):
+    guides = LocalGuide.objects.filter(city=request.GET["city"])
+    lodging = LocalLodging.objects.filter(city=request.GET["city"])
+    transportation = LocalTransportation.objects.filter(city=request.GET["city"])
+    attaction = LocalAttraction.objects.filter(city=request.GET["city"])
+    return render(request, 'travel_agency/select-trip-options.html', {''})
 
 def create_account(request):
     return render(request, 'travel_agency/base_user.html')
