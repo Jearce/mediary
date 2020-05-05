75b770b987fbe20953817c9b72a5c602ed4ed775
Jearce <jessiearce42@gmail.com>
2020-05-04 23:03:33 -0500

    Configured new urls for class based views.

--- source/edutravel/travel_agency/urls.py
+++ source/edutravel/travel_agency/urls.py
@@ -3,10 +3,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.home,name="home"),
-    path("plan/",views.plan_trip,name='plan'),
-    path("ajax/load-states/",views.load_states,name='ajax_load_states'),
-    path("ajax/load-cities/",views.load_cities,name='ajax_load_cities'),
-    path("travler-account",views.create_account,name="create"),
+    path("",
+         views.home,
+         name="home"),
+    path("plan/",
+         views.CountryListView.as_view(),
+         name='plan'),
+    path("plan/<int:country_id>/",
+         views.SubdivisionListView.as_view(),
+         name='list_subdivisions'),
+    path("plan/<int:country_id>/<int:subdivision_id>/",
+         views.HotelListView.as_view(),
+         name="list_cities"),
+    path("ajax/load-hotels/",
+         views.load_hotels,
+         name='ajax_load_hotels'),
+    path("create-traveler-account/",
+         views.create_account,
+         name="CreateTraveler"),
 ]
 
