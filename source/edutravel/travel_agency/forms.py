bab0224462f17512e5ca8af3ede3c32694ff937f
Jearce <jessiearce42@gmail.com>
2020-05-04 23:19:32 -0500

    Implemented trip form.

--- source/edutravel/travel_agency/forms.py
+++ source/edutravel/travel_agency/forms.py
@@ -2,9 +2,10 @@ from django import forms
 from .models import Trip,TripCity,Country,Subdivision,City
 
 class TripForm(forms.Form):
+
     country = forms.ModelChoiceField(queryset=Country.objects.all())
     state = forms.ModelChoiceField(queryset=Subdivision.objects.all())
-    city = forms.ModelChoiceField(queryset=City.objects.all())
+    #city = forms.ModelChoiceField(queryset=City.objects.all())
 
     #class Meta:
     #    model = Trip
@@ -13,6 +14,19 @@ class TripForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['state'].queryset = Subdivision.objects.none()
-        self.fields['city'].queryset = City.objects.none()
+        #self.fields['city'].queryset = City.objects.none()
+
+class TravelerForm(forms.Form):
+    first_name = forms.CharField(max_length=80)
+    last_name = forms.CharField(max_length=80)
+    email = forms.EmailField()
+    passport = forms.CharField(max_length=8)
+    phone_number = forms.CharField(max_length=12)
+
+    def __init__(self, *args, **kwargs):
+        super(TravelerForm,self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
 
 
